d8c85893fb7adf452c4aebcf8d512d456c99c0bb
jakkiraj <jakkiraj@brocade.com>
2016-04-23 17:49:47 -0700

    * Replaced cordova geolocation plugin with javascript geolocation plugin * Added files american state related json files * Currenlt location's state is dynamically added to the select drop down * Added temporary css changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,32 +1,83 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$ionicPlatform,$cordovaGeolocation) {
+.controller('DashCtrl', function($scope,$ionicPlatform,$cordovaGeolocation,$timeout, $ionicLoading) {
   $ionicPlatform.ready(function() {
-    console.log("hi");
+    $scope.currentState = $("#states").val();
+    console.log($scope.currentState);
 
-    
+    $("#states").on('change',function(){
+        $scope.currentState = this.value;
+    });
+
+    // Setup the loader
+    $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });
+
+    $timeout(function () {
+      $ionicLoading.hide();
+    }, 4000);
+
+
+    if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+            var coordinates={
+              "latitude": position.coords.latitude,
+              "longitude": position.coords.longitude
+            };
+            console.log(coordinates);
+
+            var geocoder = new google.maps.Geocoder();
+            var latLng = new google.maps.LatLng(coordinates.latitude, coordinates.longitude);
+
+            geocoder.geocode({'latLng': latLng},function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                      if (results[0]) {
+                          var add= results[0].formatted_address ;
+                          var value=add.split(",");
+                          count=value.length;
+                          state=value[count-2].substring(1,3);
+                          console.log(state);
+
+                          $('#states option[value='+state+']').prop('selected', true);
+
+
+                          var mapOptions = {
+                              center: latLng,
+                              zoom: 8,
+                              mapTypeId: google.maps.MapTypeId.ROADMAP
+                          };
+
+                          var image = "../img/asian_carp.png";
+
+                          $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+                          var marker = new google.maps.Marker({
+                            position: latLng,
+                            map: $scope.map,
+                            title: 'You are here!',
+                            animation: google.maps.Animation.DROP,
+                            draggable: true
+                          });
+                      }
+                      else  {
+                          alert("address not found");
+                      }
+              }
+               else {
+                  alert("Geocoder failed");
+              }
+            });
+
+            // deferred.resolve(coordinates);
+            });
+    }
+
     var posOptions = {timeout: 10000, enableHighAccuracy: false};
-    $cordovaGeolocation
-      .getCurrentPosition(posOptions)
-      .then(function (position) {
-        console.log(position);
-        $scope.lat  = position.coords.latitude;
-        $scope.long = position.coords.longitude;
-
-
-        var latLng = new google.maps.LatLng($scope.lat, $scope.long);
-
-        var mapOptions = {
-            center: latLng,
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-
-      }, function(err) {
-        // error
-      });
     });
 
 })
