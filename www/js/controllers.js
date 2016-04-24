5d3bfe04932e669695b4000bf0bae33b93a4b3e1
Dan Klos <dsklos@gmail.com>
2016-04-24 01:35:17 -0700

    Hopefully the parser is still relevant...

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,8 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$ionicPlatform,$cordovaGeolocation) {
+.controller('DashCtrl', function($scope, $ionicPlatform, $cordovaGeolocation, $http) {
   $ionicPlatform.ready(function() {
-    console.log("hi");
 
-    
     var posOptions = {timeout: 10000, enableHighAccuracy: false};
     $cordovaGeolocation
       .getCurrentPosition(posOptions)
@@ -23,6 +21,93 @@ angular.module('starter.controllers', [])
         };
         $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+        var USGSUri = 'http://waterservices.usgs.gov/nwis/iv/?format=json&bBox=-83.000000,36.500000,-81.000000,38.500000&parameterCd=00010,00060'
+
+        var determineSpawningSuitability = function(temp, flowSpike, gdd){
+          var spawningSuitability = "Unknown";
+
+          if (gdd < 650 || temp < 17) {
+            spawningSuitability = "Not suitable";
+          }
+          else if (gdd < 900){
+            if (flowSpike < .7){
+              spawningSuitability = "Minimally suitable";
+            }
+            else if (flowSpike > .7){
+              spawningSuitability = "Suitable";
+            }
+          }
+          else if (gdd > 900){
+            if (flowSpike < .7){
+              spawningSuitability = "Very suitable";
+            }
+            else if (flowSpike > .7){
+              spawningSuitability = "Highly suitable";
+            }
+          }
+          return spawningSuitability;
+        }
+
+        var findSitesWithFlowAndTemp = function(measurementSites){
+
+          var sitesWithValidFlowAndTemp = {};
+          var sitesWithFlowAndTemp = {};
+          for (var i=0; i < measurementSites.length; i++ ){
+            var validTemp = false;
+            var validFlow = false;
+            var temp;
+            var flow;
+
+            if (!sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName]){
+              sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName] = measurementSites[i];
+            }
+            else {
+            // Check if either saved site or current site has valid temp
+              if (sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].variable && sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].variable.variableName === "Temperature, water, &#176;C" && sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values && sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values[0].value && sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values[0].value[0].value !== "-999999") {
+                validTemp = true;
+                temp = sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values[0].value[0].value;
+              }
+              else if (measurementSites[i].variable.variableName === "Temperature, water, &#176;C" && measurementSites[i].values && measurementSites[i].values[0].value&& measurementSites[i].values[0].value[0].value !== "-999999") {
+                validTemp = true;
+                temp = measurementSites[i].values[0].value[0].value;
+              }
+
+              // Check if either saved site or current site has valid flow
+              if (sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].variable.variableName === "Streamflow, ft&#179;/s" && sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values && sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values[0].value && sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values[0].value[0].value !== "-999999") {
+                validFlow = true;
+                flow = sitesWithFlowAndTemp[measurementSites[i].sourceInfo.siteName].values[0].value[0].value;
+              }
+              else if (measurementSites[i].variable.variableName === "Streamflow, ft&#179;/s" && measurementSites[i].values && measurementSites[i].values[0].value && measurementSites[i].values[0].value[0].value !== "-999999") {
+                validFlow = true;
+                flow = measurementSites[i].values[0].value[0].value;
+              }
+
+            }
+
+            if (validTemp === true && validFlow === true) {
+
+              sitesWithValidFlowAndTemp[measurementSites[i].sourceInfo.siteName] = {temp: temp, flow: flow}
+            }
+          } // end for loop
+          // console.log("SITES WITH F/T:" + JSON.stringify(sitesWithValidFlowAndTemp))
+
+
+
+        }
+
+        var getUSGSData = function(){
+          $http.get(USGSUri).then(function(response){
+            USGSMeasurementSites = response.data.value.timeSeries;
+            console.log(USGSMeasurementSites);
+
+            findSitesWithFlowAndTemp(USGSMeasurementSites);
+
+          }, function(error){
+            console.log("ERROR: " + error)
+          });
+        }
+
+        getUSGSData();
 
       }, function(err) {
         // error
